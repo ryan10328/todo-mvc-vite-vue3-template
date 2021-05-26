60be5f3bc5f65ffa7c3d549f27060f7164e30384
Ryan Tseng <ryan10328@gmail.com>
2021-05-27 02:13:04 +0800

    finished footer's filter, clear function

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       <ul class="todo-list">
         <!-- These are here just to show the structure of the list items -->
         <!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
-        <li :class="{ completed: item.done }" v-for="(item, index) in tasks" :key="index">
+        <li :class="{ completed: item.done }" v-for="(item, index) in filteredTasks" :key="index">
           <div class="view">
             <input class="toggle" type="checkbox" v-model="item.done" />
             <label>{{item?.text}}</label>
@@ -19,7 +19,11 @@
       </ul>
     </section>
     <!-- This footer should be hidden by default and shown when there are todos -->
-    <AppFooter :items-left="itemsLeft"></AppFooter>
+    <AppFooter
+      :items-left="itemsLeft"
+      v-on:selected:change="onSelectedChange($event)"
+      v-on:clear:clicked="onClearClicked()"
+    ></AppFooter>
   </section>
 </template>
 
@@ -27,14 +31,16 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
 import AppFooter from "./components/AppFooter.vue";
-import { computed, reactive, toRefs } from "vue";
+import { computed, reactive, toRefs, watch, ref } from "vue";
 export default {
   components: {
     AppHeader,
     AppFooter,
   },
   setup() {
+    const filteredTasks = ref([]);
     const obj = reactive({
+      flag: "",
       tasks: [],
     });
 
@@ -50,12 +56,37 @@ export default {
       obj.tasks.splice(index, 1);
     };
 
+    const onSelectedChange = (evt) => {
+      obj.flag = evt;
+    };
+
+    const onClearClicked = () => {
+      obj.tasks = obj.tasks.filter((g) => !g.done);
+    };
+
+    watch(obj, (val, newVal) => {
+      switch (newVal.flag) {
+        case "all":
+          filteredTasks.value = obj.tasks;
+          return;
+        case "active":
+          filteredTasks.value = obj.tasks.filter((g) => !g.done);
+          return;
+        case "completed":
+          filteredTasks.value = obj.tasks.filter((g) => g.done);
+          return;
+      }
+    });
+
     const itemsLeft = computed(() => {
       return obj.tasks.filter((g) => !g.done).length;
     });
 
     return {
       onTaskCreate,
+      onSelectedChange,
+      onClearClicked,
+      filteredTasks,
       removeTask,
       itemsLeft,
       ...toRefs(obj),
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,27 +7,52 @@
     <!-- Remove this if you don't implement routing -->
     <ul class="filters">
       <li>
-        <a class="selected" href="#/">All</a>
+        <a :class="{selected: selectedItem === 'all'}" :href="void(0)" @click="check('all')">All</a>
       </li>
       <li>
-        <a href="#/active">Active</a>
+        <a
+          :class="{selected: selectedItem === 'active'}"
+          :href="void(0)"
+          @click="check('active')"
+        >Active</a>
       </li>
       <li>
-        <a href="#/completed">Completed</a>
+        <a
+          :class="{selected: selectedItem === 'completed'}"
+          :href="void(0)"
+          @click="check('completed')"
+        >Completed</a>
       </li>
     </ul>
     <!-- Hidden if no completed items are left ↓ -->
-    <button class="clear-completed">Clear completed</button>
+    <button class="clear-completed" @click="clearComplete">Clear completed</button>
   </footer>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   props: {
     itemsLeft: Number,
   },
   setup(props, ctx) {
-    return {};
+    const selectedItem = ref("all");
+    ctx.emit("selected:change", selectedItem.value);
+
+    const check = (flag) => {
+      selectedItem.value = flag;
+      ctx.emit("selected:change", selectedItem.value);
+    };
+
+    const clearComplete = () => {
+      ctx.emit("clear:clicked");
+    };
+
+    return {
+      selectedItem,
+      check,
+      clearComplete,
+    };
   },
 };
 </script>
